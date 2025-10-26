259c494a79e706226b5d3665fdfb9ed9ef90d80d
bula393 <alumno26.buhler.santiago@ipm.edu.ar>
2025-10-26 11:59:45 -0300

    a y b terminado, mayoria de la base armada

--- polimark/src/main/java/com/polimark/polimark/service/CompraService.java
+++ polimark/src/main/java/com/polimark/polimark/service/CompraService.java
@@ -7,13 +7,15 @@ public class CompraService {
     private final FuncionRepository funcionRepository;
     private final UsuarioRepository usuarioRepository;
     private final MetodoPagoRepository metodoPagoRepository;
+    private final CompraRepository compraRepository;
 
     public CompraService(FuncionRepository funcionRepository,UsuarioRepository usuarioRepository
-    MetodoPagoRepository metodoPagoRepository)
+    MetodoPagoRepository metodoPagoRepository,CompraRepository compraRepository)
         {
             this.funcionRepository = funcionRepository;
             this.usuarioRepository = usuarioRepository;
             this.metodoPagoRepository = metodoPagoRepository;
+            this.compraRepository = compraRepository;
         }
     public void reservarCompra(int idFuncion,int identificadorUsuario,int idmetodoPago,List<butaca> butacas) {
         Funcion funcion = funcionRepository.findById(funcionId)
@@ -31,11 +33,33 @@ public class CompraService {
             for(Butaca butaca : butacas){
                     entradaService.reservarAsiento(nuevaCompra,funcion,butaca);
                 }
+            compraRepository.save(nuevaCompra);
         }
         else{
             throw new excedeLimiteExeption();
         }
     }
 
+    public void pagarCompra() {
+        Funcion funcion = funcionRepository.findById(funcionId)
+                .orElseThrow(() -> new RuntimeException("Función no encontrada"));
 
+        Usuario usuario = usuarioRepository.findById(identificadorUsuario)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        MetodoPago metodoPago = metodoPagoRepository.findById(idmetodoPago)
+        .orElseThrow(() -> new RuntimeException("Metodo de Pago no encontrada"));
+
+
+        if(butacas.size() > 6){
+            Compra nuevaCompra = new Compra(false,usuario,metodoPago);
+            for(Butaca butaca : butacas){
+                    entradaService.reservarAsiento(nuevaCompra,funcion,butaca);
+                }
+            compraRepository.save(nuevaCompra);
+        }
+        else{
+            throw new excedeLimiteExeption();
+        }
+    }
 }
