0fd4ebcb6a2414ed7fc1385f1e2736f1e8eb96c2
bula393 <alumno26.buhler.santiago@ipm.edu.ar>
2025-11-23 17:16:27 -0300

    subidos jsons

--- polimark/src/main/java/com/api/Polimark/controler/ButacaController.java
+++ polimark/src/main/java/com/api/Polimark/controler/ButacaController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("git ")
+@RequestMapping("/api/butaca")
 @CrossOrigin(origins = "*")
 public class ButacaController {
 
