57b4e7aa303413676df90c80ead6a660573aba57
bula393 <alumno26.buhler.santiago@ipm.edu.ar>
2025-11-24 21:16:34 -0300

    ultimo commit

--- polimarkDatabase.sql
+++ polimarkDatabase.sql
@@ -508,7 +508,7 @@ DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
-  `identificador` int NOT NULL AUTO_INCREMENT,
+  `identificador` int NOT NULL ,
   `nombre` varchar(45) DEFAULT NULL,
   `apellido` varchar(45) DEFAULT NULL,
   `mail` varchar(45) DEFAULT NULL,
